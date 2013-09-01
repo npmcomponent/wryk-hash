2a9f79cf436285352c6b7fa1aa47feaa03a5d5cb
Mathieu Gallé-Tessonneau <mathieu.galletessonneau@gmail.com>
2013-09-02 00:34:51 +0200

    v0.1.0

---
+++ index.js
@@ -0,0 +1,131 @@
+var properties = require('properties');
+module.exports = Hash;
+
+function Hash (object) {
+	this._entries = {};
+
+	for (var key in object) {
+		if (object.hasOwnProperty(key)) {
+			this.set(key, object[key]);
+		}
+	}
+}
+
+Hash.prototype.get = function (key) {
+	return this._entries[key];
+};
+
+Hash.prototype.set = function (key, value) {
+	this._entries[key] = value;
+};
+
+Hash.prototype.delete = function (key) {
+	if (this.has(key)) {
+		delete this._entries[key];
+		return true;
+	}
+
+	return false;
+};
+
+Hash.prototype.has = function (key) {
+	return this._entries.hasOwnProperty(key);
+};
+
+Hash.prototype.clear = function () {
+	var entries = this._entries;
+	for (var key in entries) {
+		if (entries.hasOwnProperty(key)) {
+			delete entries[key];
+		}
+	}
+};
+
+Hash.prototype.forEach = function (callback, context) {
+	var entries = this._entries;
+	for (var key in entries) {
+		if (entries.hasOwnProperty(key)) {
+			callback.call(context, entries[key], key, this);
+		}
+	}
+};
+
+Hash.prototype.keys = function () {
+	return new HashIterator(this, 'key');
+};
+
+Hash.prototype.values = function () {
+	return new HashIterator(this, 'value');
+};
+
+Hash.prototype.entries= Hash.prototype.__iterator__ = function () {
+	return new HashIterator(this, 'key+value');
+};
+
+Hash.prototype.iterator  = function (kind) {
+	return new HashIterator(this, kind);
+};
+
+
+properties(Hash.prototype)
+	.default({ configurable: true })
+	.property('get').value().define()
+	.property('set').value().define()
+	.property('delete').value().define()
+	.property('has').value().define()
+	.property('clear').value().define()
+	.property('forEach').value().define()
+	.property('size')
+		.getter(function () {
+			var counter = 0, entries = this._entries;
+			for (var key in entries) {
+				if (entries.hasOwnProperty(key)) { counter++; }
+			}
+			return counter;
+		}).define()
+	.property('keys').value().define()
+	.property('values').value().define()
+	.property('entries').value().define()
+	.property('iterator').value().define()
+	.property('__iterator__').value().define()
+;
+
+
+
+
+
+
+
+function HashIterator (hash, kind) {
+	this._entries = hash._entries;
+	this._kind = kind;
+	this._index = 0;
+	this._keys = [];
+
+	for (var property in hash._entries) {
+		this._keys.push(property);
+	}
+}
+
+HashIterator.prototype.next = function () {
+	var key = this._keys[this._index] !== undefined ? this._keys[this._index] : null;
+	var value = this._entries[this._keys[this._index]] !== undefined ? this._entries[this._keys[this._index]] : null;
+
+	this._index++;
+			
+	switch (this._kind) {
+		case 'key':
+			return key;
+		break;
+		case 'value':
+			return value;
+		break;
+		case 'key+value':
+			return (key !== null || value !== null) ? [key, value] : null;
+	}
+};
+
+properties(HashIterator.prototype)
+	.default({ configurable: true })
+	.property('next').value().define()
+;
---
+++ test/index.ls
@@ -0,0 +1,104 @@
+require! {
+	Hash: '../build/hash'
+	should
+}
+
+describe "new Hash(object)" (...) !->
+	h = new Hash do
+		'a': 'aa'
+		'b': 'bb'
+
+	it "should initialize key/value entries from object" !->
+		h.get 'a' .should.be.equal 'aa'
+		h.get 'b' .should.be.equal 'bb'
+
+	describe ".get(key)" (...) !->
+		it "should return the key value or undefined" !->
+			h.get 'a' .should.be.equal 'aa'
+			should.not.exist h.get 'z'
+
+	describe ".set(key)" (...) !->
+		it "should set a new key/value entry or override existing entry" !->
+			h.set 'c' 'cc'
+			h.get 'c' .should.be.equal 'cc'
+
+
+	describe ".delete(key)" (...) !->
+		it "should remove a new key/value entry" !->
+			should.exist h.get 'c'
+			h.delete 'c'
+			should.not.exist h.get 'c'
+
+		it "should return true if an entry are deleted" !->
+			h.set 'c' 'cc'
+			h.delete 'c' .should.be.true
+
+		it "should return false if an entry not are deleted" !->
+			h.delete 'c' .should.be.false
+
+
+	describe ".has(key)" (...) !->
+		it "should return true if key/value entry is defined" !->
+			h.has 'a' .should.be.true
+
+		it "should return false if key/value entry is not defined" !->
+			h.has 'z' .should.be.false
+
+
+	describe ".clear()" (...) !->
+		it "should delete all key/value entries" !->
+			h.clear!
+			h.size.should.be.equal 0
+
+
+	describe ".size" (...) !->
+		it "should return the number of entries" !->
+			h.size.should.be.equal 0
+			h.set 'a' 'aa'
+			h.size.should.be.equal 1
+
+	describe ".forEach(callback [, context])" (...) !->
+		it "should call callback for all entries with (value, key, hash)" !->
+			h.set 'b' 'bb'
+			h.set 'c' 'cc'
+
+			counter = 0
+			h.forEach (value, key, hash) !->
+				counter++
+				value.should.be.equal h.get key
+				hash.should.be.equal h
+
+			counter.should.be.equal h.size
+
+		it "should use context as current context" !->
+			context = {}
+			h.forEach do
+				!-> @should.be.equal context
+				context
+
+	describe ".keys()" (...) !->
+		it "should return a hash keys iterator" !->
+			iterator = h.keys!
+
+			while (key = iterator.next!) != null
+				h.has key .should.be.true
+
+	describe ".values()" (...) !->
+		it "should return a hash values iterator" !->
+			iterator = h.values!
+
+			while (value = iterator.next!) != null
+				found = false
+
+				h.forEach !-> found := true if it == value
+
+				found.should.be.true
+				
+
+	describe ".entries()" (...) !->
+		it "should return a hash entries iterator" !->
+			iterator = h.entries!
+
+			while (entry = iterator.next!) != null
+				[key, value] = entry
+				h.get key .should.be.equal value
